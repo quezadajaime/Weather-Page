495b550f3fa779e3e6bd0677a7746e8bddacafd7
Jaime <quezadajaime@ymail.com>
2023-08-12 10:38:24 -0700

    Got current and 5 day forecast working

--- script.js
+++ script.js
@@ -1,101 +1,132 @@
-var recentSearches = JSON.parse(localStorage.getItem("searches")) ||[];
+var recentSearches = JSON.parse(localStorage.getItem("searches")) || [];
 console.log(recentSearches);
-var apiKey="949e4d910374477340798e31903cc1ea";
-
+//var apiKey="949e4d910374477340798e31903cc1ea";
 var searchHistory = document.getElementById("recent-viewed");
 
 getWeather("Riverside")
 
 function getWeather(city) {
-var url = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${apiKey}`;
-console.log();
-fetch (url)
-	.then(function (response) {
-	return response.json();
-})
-	.then(function (data) {
-	console.log(data);
-	localStorage.setItem("city", JSON.stringify(data)
-);
+    var url = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=949e4d910374477340798e31903cc1ea&units=imperial";
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            localStorage.setItem("city", JSON.stringify(data)
+            );
 
-	console.log(data)
-	var currentCity = document.getElementById("current-city");
+            console.log(data)
+            var currentCity = document.getElementById("current-city");
 
-	currentCity.textContent = city;
-	setCurrentWeather(data.list[0]);
-	setForecast(data.list);
-});
+            currentCity.textContent = city;
+            setCurrentWeather(data.list[0]);
+            setForecast(data.list);
+        });
 }
 
 
 function setCurrentWeather(weather) {
-var forecastDate = weather.dt;
-console.log(forecastDate);
-var convertTimeMilli = forecastDate * 1000;
-var dateTime = new Date(convertTimeMilli);
-var newDate = dateTime.toLocalDateString("en-US", dateStyle, "short");
-console.log(newDate);
-var currentDate = document.getElementById( "current-date");
-currentDate.innerHTML = newDate;
+    var forecastDate = weather.dt;
+    var convertTimeMilli = forecastDate * 1000;
+    var dateTime = new Date(convertTimeMilli);
+    var newDate = dateTime.toLocaleDateString("en-US", { dateStyle: "short" });
+    var currentDate = document.getElementById("current-date");
+    currentDate.innerHTML = newDate;
+    var iconParagraphId = weather.weather[0].icon;
+    var iconLink = "https://openweathermap.org/img/wn/" + iconParagraphId + ".png";
+    var iconHTML = '<img src="' + iconLink + '">';
 
-var iconParagraphId = weather.weather[0].icon;
-variconLink = "https://openweathermap.org/img/wn/" + iconParagraphId + ".png";
 
-console.log(iconLink);
+    var currentIcon = document.getElementById("current-icon");
+    currentIcon.innerHTML = iconHTML;
 
-var iconHTML = '<img scr="' + iconLink + '">';
-console.log(iconHTML);
+    var currentTemperature = document.getElementById("current-temperature");
+    currentTemperature.textContent = "Temperature: " + weather.main.temp;
+    var currentWind = document.getElementById("current-wind");
+    currentWind.textContent = "Wind: " + weather.wind.speed + "MPH";
 
-var currentIcon = document.getElementById("current-icon");
-
-currentIcon.innerHTML = iconHTML;
-
-console.log(weather.main.temp);
-
-var currentTemperature = document.getElementById("current-temperature");
-
-var currentWind = document.getElementById("current-wind");
-	currentWind.textContent = "wind: " + weather.wind.speed + "MPH";
-
-var currentHumidity = document.getElementById("current-humidity");
-	currentHumidity.textContent = "humidity: " + weather.main.humidity + " %";
+    var currentHumidity = document.getElementById("current-humidity");
+    currentHumidity.textContent = "Humidity: " + weather.main.humidity + " %";
 }
 
 
 function setForecast(forecast) {
-	for (var i = 0; i < forecast.length; i += 8) {
-	console.log(forecast[i]);
-	setForecastDay(forecast[i], i/ 8 + 1);
-  }
+    for (var i = 0; i < forecast.length; i += 8) {
+        console.log(forecast[i]);
+        setForecastDay(forecast[i], i / 8 + 1);
+    }
 }
-
+//5 Day forecast
 function setForecastDay(weather, dayNumber) {
 
-var iconParagraph = document.createElement("p");
-var iconParagraphId = weather.weather[0].icon;
-var iconLink = "https   " + iconParagraphId + ".png";
-	console.log(iconLink);
-var iconHtml = '<img src="' + iconLink + '">';
-	console.log(iconHTML);
-var forecastParagraph = document.createElement("p");
-var forecastDate = weather.dt;
-console.log(forecastDate);
-var convertTimeMilli = forecaseDate * 1000;
-var dateTime = new Date(convertTimeMilli);
-var newDate = dateTime.toLocalDataString("en-US", {
-dataStyle: "short" });
-	console.log(newDate);
-var day = doument.getElementById("day-" + dayNumber);
+    var iconParagraph = document.createElement("p");
+    var iconParagraphId = weather.weather[0].icon;
+    var iconLink = "https://openweathermap.org/img/wn/" + iconParagraphId + ".png";
+    //console.log(iconLink);
 
-var dayList = [];
-var titleParagraph = dcument.createElement("p");
-	console.log(iconHTML);
-titleParagraph.innerHTML = newDate + iconHTML;
-dayList.push(titleParagraph);
-day.replaceChildren(...dayList);
+    var iconHTML = '<img src="' + iconLink + '">';
+    //console.log(iconHTML);
 
-var windParagraph = document.createElement("p");
-windParagraph.textContent = "wind: "+ weather.wind.speed + " MPH";
-	dayList.push(windParagraph);
-	day
+    var forecastParagraph = document.createElement("p");
+    var forecastDate = weather.dt;
+    //console.log(forecastDate);
+
+    var convertTimeMilli = forecastDate * 1000;
+    var dateTime = new Date(convertTimeMilli);
+    var newDate = dateTime.toLocaleDateString("en-US", {dateStyle: "short"});
+    //console.log(newDate);
+
+    var day = document.getElementById("day-" + dayNumber);
+
+    var dayList = [];
+    var titleParagraph = document.createElement("p");
+    console.log(iconHTML);
+    titleParagraph.innerHTML = newDate + iconHTML;
+    dayList.push(titleParagraph);
+    day.replaceChildren(...dayList);
+
+    //Temp data
+    var tempParagraph = document.createElement("p");
+    tempParagraph.textContent = "Temp: " + weather.main.temp;
+    dayList.push(tempParagraph);
+    day.replaceChildren(...dayList);
+    //Wind data
+
+    var windParagraph = document.createElement("p");
+    windParagraph.textContent = "wind: " + weather.wind.speed + " MPH";
+    dayList.push(windParagraph);
+    day.replaceChildren(...dayList);
+   
+    //humidity
+    var humidityParagraph = document.createElement("p");
+    humidityParagraph.textContent = "Humidity: " + weather.main.humidity + " %";
+    dayList.push(humidityParagraph);
+    day.replaceChildren(...dayList);
 }
+
+var searchForm = document.getElementById("search-form");
+var city = document.getElementById("city");
+searchForm.addEventListener("search", function (event) {
+    event.preventDefault();
+    var searchCity = city.value.trim();
+    executeSearch(searchCity);
+    addRecentSearch(searchCity);
+});
+
+function addRecentSearch(city) {
+    var recentButton = document.createElement("button");
+    recentButton.textContent = city;
+    recentButton.addEventListener("click", function() {
+        executeSearch(city);
+    });
+    searchHistory.appendChild(recentButton);
+}
+
+function executeSearch(searchCity) {
+    recentSearches.push(searchCity);
+    localStorage.setItem("searches", JSON.stringify(recentSearches));
+    getWeather(searchCity);
+}
+
+
